e09f36c60c9a299c7760bb6b1c952558d1c5d23d
Greg Michnikov <greg.michnikov@gmail.com>
2016-10-18 15:28:34 -0400

    Finish refactoring board.rb

--- board.rb
+++ board.rb
@@ -5,6 +5,8 @@ Dir["./pieces/*"].each {|file| require_relative file }
 
 class Board
 
+  PIECE_IN_COLUMN = { 0 => Rook, 1 => Knight, 2 => Bishop, 3 => Queen, 4 => King, 5 => Bishop, 6 => Knight, 7 => Rook }
+
   attr_accessor :rows
 
   def self.in_bounds?(pos)
@@ -25,8 +27,8 @@ class Board
       move!(from_pos, to_pos)
       made_move = true
     rescue ChessError => e
-      puts "Error: #{e.message}"
-      sleep(0.75)
+      puts "#{e.message}.\nPlease try again."
+      sleep(1)
     end
     made_move
   end
@@ -75,10 +77,10 @@ class Board
   end
 
   def in_check?(color)
-    other_color = color == :white ? :black : :white
-    king = self.rows.flatten.select { |piece| piece.is_a?(King) &&
-      piece.color == color}.first
-    opponent_pieces = self.rows.flatten.select { |piece| piece.color == other_color }
+    opponent_color = color == :white ? :black : :white
+    king = self.rows.flatten.find { |piece| piece.is_a?(King) &&
+      piece.color == color }
+    opponent_pieces = self.rows.flatten.select { |piece| piece.color == opponent_color }
     opponent_pieces.any? { |piece| piece.moves.include?(king.pos) }
   end
 
@@ -94,43 +96,30 @@ class Board
   private
 
   def make_starting_grid
+    place_black_pieces
+    mark_middle_rows_as_empty
+    place_white_pieces
+  end
 
-    self[[0,0]] = Rook.new(:black, self, [0,0])
-    self[[0,1]] = Knight.new(:black, self, [0,1])
-    self[[0,2]] = Bishop.new(:black, self, [0,2])
-    self[[0,3]] = Queen.new(:black, self, [0,3])
-    self[[0,4]] = King.new(:black, self, [0,4])
-    self[[0,5]] = Bishop.new(:black, self, [0,5])
-    self[[0,6]] = Knight.new(:black, self, [0,6])
-    self[[0,7]] = Rook.new(:black, self, [0,7])
-    self.rows.each_with_index do |row, r_index|
-      row.each_with_index do |square, c_index|
-        if r_index == 0
-          next
-        elsif r_index == 1
-          self[[r_index, c_index]] = Pawn.new(:black, self, [r_index, c_index])
-        elsif r_index.between?(2,5)
-          self[[r_index, c_index]] = NullPiece.instance
-        elsif r_index == 6
-          self[[r_index, c_index]] = Pawn.new(:white, self, [r_index, c_index])
-        else
-        end
-      end
+  def place_black_pieces
+    (0..7).each { |col| self[[0, col]] = PIECE_IN_COLUMN[col].new(:black, self, [0, col]) }
+    (0..7).each { |col| self[[1, col]] = Pawn.new(:black, self, [1, col]) }
+  end
+
+  def mark_middle_rows_as_empty
+    (2..5).each do |row|
+      (0..7).each { |col| self[[row, col]] = NullPiece.instance }
     end
-    self[[7,0]] = Rook.new(:white, self, [7,0])
-    self[[7,1]] = Knight.new(:white, self, [7,1])
-    self[[7,2]] = Bishop.new(:white, self, [7,2])
-    self[[7,3]] = Queen.new(:white, self, [7,3])
-    self[[7,4]] = King.new(:white, self, [7,4])
-    self[[7,5]] = Bishop.new(:white, self, [7,5])
-    self[[7,6]] = Knight.new(:white, self, [7,6])
-    self[[7,7]] = Rook.new(:white, self, [7,7])
+  end
 
+  def place_white_pieces
+    (0..7).each { |col| self[[6, col]] = Pawn.new(:white, self, [6, col]) }
+    (0..7).each { |col| self[[7, col]] = PIECE_IN_COLUMN[col].new(:white, self, [7, col]) }
   end
 
   def check_for_move_errors(current_piece, current_player_color, to_pos)
     raise ChessError.new("Starting position is empty") if current_piece.is_a?(NullPiece)
-    raise ChessError.new("Cannot move oppponent's piece") if current_piece.color != current_player_color
+    raise ChessError.new("Cannot move opponent's piece") if current_piece.color != current_player_color
     raise ChessError.new("Cannot move to that position") unless Board.in_bounds?(to_pos)
     raise ChessError.new("Invalid move for that piece") unless current_piece.valid_moves.include?(to_pos)
   end
