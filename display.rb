1ccc2e34480fad13b43462d51a1b9ef044178f24
Greg Michnikov <greg.michnikov@gmail.com>
2016-10-18 16:15:03 -0400

    Finish refactoring display

--- display.rb
+++ display.rb
@@ -8,8 +8,8 @@ class Display
   P2COLOR = :black
   BCOLOR1 = :green
   BCOLOR2 = :light_white
-  CURSOR_BCOLOR = :blue
   CURSOR_COLOR = :yellow
+  POSSIBLE_MOVE_COLOR = :blue
 
   attr_accessor :cursor, :board
 
@@ -20,31 +20,36 @@ class Display
 
   def render(possible_destinations = [])
     self.board.rows.each_with_index do |row, r_index|
-      row.each_with_index do |square, c_index|
-        # background_color = determine_background_color(r_index, c_index)
-        # color = determine_color(square)
+      row.each_with_index do |piece, c_index|
+        pos = [r_index, c_index]
+        background_color = determine_background_color(pos)
+        color = determine_color(piece)
 
-        if self.cursor.cursor_pos == [r_index, c_index]
-          print "#{square} ".colorize(:color => CURSOR_COLOR, :background => CURSOR_BCOLOR).blink
-        elsif possible_destinations.include?([r_index, c_index])
-          print "#{square} ".colorize(:background => CURSOR_COLOR)
-        elsif (r_index + c_index).even? && square.is_a?(NullPiece)
-          print "#{square} ".colorize(:background => BCOLOR1)
-        elsif (r_index + c_index).odd? && square.is_a?(NullPiece)
-          print "#{square} ".colorize(:background => BCOLOR2)
-        elsif (r_index + c_index).even? && square.color == :black
-          print "#{square} ".colorize(:color => P2COLOR, :background => BCOLOR1)
-        elsif (r_index + c_index).even? && square.color == :white
-          print "#{square} ".colorize(:color => P1COLOR, :background => BCOLOR1)
-        elsif (r_index + c_index).odd? && square.color == :black
-          print "#{square} ".colorize(:color => P2COLOR, :background => BCOLOR2)
-        elsif (r_index + c_index).odd? && square.color == :white
-          print "#{square} ".colorize(:color => P1COLOR, :background => BCOLOR2)
+        if contains_cursor?(pos)
+          print "#{piece} ".colorize(:color => color, :background => CURSOR_COLOR).blink
+        elsif possible_destinations.include?(pos)
+          print "#{piece} ".colorize(:color => color, :background => POSSIBLE_MOVE_COLOR)
+        else
+          print "#{piece} ".colorize(:color => color, :background => background_color)
         end
 
       end
       puts
     end
+  end
+
+  private
+
+  def determine_background_color(pos)
+    (pos[0] + pos[1]).even? ? BCOLOR1 : BCOLOR2
+  end
+
+  def determine_color(piece)
+    piece.color == :white ? P1COLOR : P2COLOR
+  end
+
+  def contains_cursor?(pos)
+    self.cursor.cursor_pos == pos
   end
 
 
